f09796f1426c1295581f1d77670ba4a46000eae6
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-01-09 18:20:56 -0300

    feat: create Iconoir component for icons

---
+++ web/src/components/iconoir.rs
@@ -0,0 +1,16 @@
+use dioxus::prelude::*;
+
+#[derive(Props, Clone, Debug, PartialEq, Eq)]
+pub struct IconoirProps {
+    icon: String,
+    class: Option<String>,
+    id: Option<String>,
+    style: Option<String>,
+}
+
+#[component]
+pub fn Iconoir(props: IconoirProps) -> Element {
+    rsx! {
+        i { class: format!("iconoir-{} {}", props.icon, props.class.unwrap_or_default()), id: props.id.unwrap_or_default(), style: props.style.unwrap_or_default() }
+    }
+}
---
+++ web/src/components/mod.rs
@@ -0,0 +1,3 @@
+mod iconoir;
+
+pub use iconoir::*;
