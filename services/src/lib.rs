f1bda0ea55bc7e23925dd76700e277768e68136d
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-01-09 14:13:01 -0300

    feat: create Service Trait and ServiceManager

--- services/src/lib.rs
+++ services/src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::{
+    any::{Any, TypeId},
+    collections::HashMap,
+    sync::Arc,
+};
+
+use thiserror::Error;
+use tokio::sync::RwLock;
+
+pub type ServiceResult<T> = Result<T, ServiceError>;
+
+#[async_trait::async_trait]
+pub trait Service: Sync + Send {
+    type Input: Sync + Send;
+    type Result: Sync + Send;
+
+    async fn run(&self, dto: Self::Input) -> ServiceResult<Self::Result>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct ServiceManager {
+    map: RwLock<HashMap<TypeId, Arc<dyn Any + Sync + Send>>>,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl ServiceManager {
+    pub fn new() -> Self {
+        Self {
+            map: RwLock::new(HashMap::new()),
+        }
+    }
+
+    pub async fn register<S: Service + 'static>(&self, service: S) {
+        let mut map = self.map.write().await;
+        map.insert(TypeId::of::<S>(), Arc::new(service));
+    }
+
+    pub async fn get<S: Service + 'static>(&self) -> Option<Arc<S>> {
+        let map = self.map.read().await;
+
+        if let Some(service_any) = map.get(&TypeId::of::<S>()) {
+            let s = service_any.clone();
+            return s.downcast().ok();
+        }
+
+        None
     }
 }
+
+#[derive(Error, Debug)]
+pub enum ServiceError {}
--- web/src/main.rs
+++ web/src/main.rs
@@ -1,5 +1,7 @@
 use dioxus::prelude::*;
 
+mod pages;
+
 #[derive(Debug, Clone, Routable, PartialEq)]
 #[rustfmt::skip]
 enum Route {
