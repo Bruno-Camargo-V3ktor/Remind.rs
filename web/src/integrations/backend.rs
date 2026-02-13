82555aea15730eaaf51ef3d0228c8a489bd7fef7
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-02-12 23:16:52 -0300

    feat: implematation for auth user in frontend

--- web/src/integrations/backend.rs
+++ web/src/integrations/backend.rs
@@ -1,8 +1,10 @@
-use dtos::{CreateUserDTO, LoginUserDTO};
+use dtos::{CreateUserDTO, InfoUserDTO, LoginUserDTO};
 use http::error::ErrorInfos;
+use serde::Serialize;
 
 const BASE_URL: &str = "http://localhost:3000/api";
 
+#[derive(Clone, Debug)]
 pub struct Token(pub String);
 
 #[derive(Default, Debug)]
@@ -95,6 +97,39 @@ impl Backend {
                 .unwrap()
                 .to_string();
             Ok(Token(token_str))
+        } else {
+            Err(http_response.error.unwrap())
+        }
+    }
+
+    pub async fn auth_user(&self, token: Token) -> Result<InfoUserDTO, ErrorInfos> {
+        let response = self
+            .client
+            .get(format!("{BASE_URL}/auth/user"))
+            .header("Authorization", token.0)
+            .send()
+            .await
+            .map_err(|e| {
+                ErrorInfos::new(
+                    "REQWEST_ERROR".into(),
+                    "Failed to send request".into(),
+                    e.to_string(),
+                )
+            })?;
+
+        let http_response: http::Response = response.json().await.map_err(|e| {
+            ErrorInfos::new(
+                "SERIALIZATION_ERROR".into(),
+                "Failed to parse response".into(),
+                e.to_string(),
+            )
+        })?;
+
+        if http_response.success {
+            let value = http_response.data.as_ref().unwrap().clone();
+            let infos: InfoUserDTO = serde_json::from_value(value).unwrap();
+
+            Ok(infos)
         } else {
             Err(http_response.error.unwrap())
         }
