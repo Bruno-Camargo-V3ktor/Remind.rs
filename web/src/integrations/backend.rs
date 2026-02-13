86553370863cc428bfd05c3474faabaa3596f87b
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-02-12 23:24:31 -0300

    feat: implematation for update user in frontend

--- web/src/integrations/backend.rs
+++ web/src/integrations/backend.rs
@@ -1,4 +1,4 @@
-use dtos::{CreateUserDTO, InfoUserDTO, LoginUserDTO};
+use dtos::{CreateUserDTO, InfoUserDTO, LoginUserDTO, UpdateUserDTO};
 use http::error::ErrorInfos;
 use serde::Serialize;
 
@@ -160,6 +160,44 @@ impl Backend {
 
         if http_response.success {
             Ok(())
+        } else {
+            Err(http_response.error.unwrap())
+        }
+    }
+
+    pub async fn update_user(
+        &self,
+        token: Token,
+        update: UpdateUserDTO,
+    ) -> Result<InfoUserDTO, ErrorInfos> {
+        let response = self
+            .client
+            .put(format!("{BASE_URL}/users/"))
+            .header("Authorization", token.0)
+            .json(&update)
+            .send()
+            .await
+            .map_err(|e| {
+                ErrorInfos::new(
+                    "REQWEST_ERROR".into(),
+                    "Failed to send request".into(),
+                    e.to_string(),
+                )
+            })?;
+
+        let http_response: http::Response = response.json().await.map_err(|e| {
+            ErrorInfos::new(
+                "SERIALIZATION_ERROR".into(),
+                "Failed to parse response".into(),
+                e.to_string(),
+            )
+        })?;
+
+        if http_response.success {
+            let value = http_response.data.as_ref().unwrap().clone();
+            let infos: InfoUserDTO = serde_json::from_value(value).unwrap();
+
+            Ok(infos)
         } else {
             Err(http_response.error.unwrap())
         }
