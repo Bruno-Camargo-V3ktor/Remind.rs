5fb326a9d82549e5d44aca672345c4f669d4d3f3
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-02-12 23:40:39 -0300

    feat: implematation for create property in frontend

--- web/src/integrations/backend.rs
+++ web/src/integrations/backend.rs
@@ -1,5 +1,5 @@
 use domain::models::Property;
-use dtos::{CreateUserDTO, InfoUserDTO, LoginUserDTO, UpdateUserDTO};
+use dtos::{CreatePropertyDTO, CreateUserDTO, InfoUserDTO, LoginUserDTO, UpdateUserDTO};
 use http::error::ErrorInfos;
 
 const BASE_URL: &str = "http://localhost:3000/api";
@@ -262,6 +262,44 @@ impl Backend {
             let propertys: Vec<Property> = serde_json::from_value(value).unwrap();
 
             Ok(propertys)
+        } else {
+            Err(http_response.error.unwrap())
+        }
+    }
+
+    pub async fn create_property(
+        &self,
+        token: Token,
+        created: CreatePropertyDTO,
+    ) -> Result<Property, ErrorInfos> {
+        let response = self
+            .client
+            .post(format!("{BASE_URL}/propertys/"))
+            .header("Authorization", token.0)
+            .json(&created)
+            .send()
+            .await
+            .map_err(|e| {
+                ErrorInfos::new(
+                    "REQWEST_ERROR".into(),
+                    "Failed to send request".into(),
+                    e.to_string(),
+                )
+            })?;
+
+        let http_response: http::Response = response.json().await.map_err(|e| {
+            ErrorInfos::new(
+                "SERIALIZATION_ERROR".into(),
+                "Failed to parse response".into(),
+                e.to_string(),
+            )
+        })?;
+
+        if http_response.success {
+            let value = http_response.data.as_ref().unwrap().clone();
+            let property: Property = serde_json::from_value(value).unwrap();
+
+            Ok(property)
         } else {
             Err(http_response.error.unwrap())
         }
