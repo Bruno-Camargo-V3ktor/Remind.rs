c9f936d2f1efea87c1b574496a98c8dd4a77a1aa
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2026-02-24 23:16:17 -0300

    feat: finish logic for login in frontnd

--- web/src/components/button.rs
+++ web/src/components/button.rs
@@ -1,11 +1,22 @@
 use dioxus::prelude::*;
 
+#[derive(Debug, Default, Clone, PartialEq)]
+pub enum IconSide {
+    #[default]
+    Left,
+    Right,
+}
+
 #[derive(Props, Debug, Clone, PartialEq)]
 pub struct ButtonProps {
     icon: Element,
+    #[props(default)]
+    icon_side: IconSide,
     text: String,
     #[props(default = false)]
     inverse: bool,
+    #[props(default = false)]
+    disable: bool,
     onclick: EventHandler,
 }
 
@@ -19,9 +30,16 @@ pub fn Button(props: ButtonProps) -> Element {
 
     rsx! {
         div { class: "button-container",
-            button { class: "button-content {color_class}", onclick: move |e| {e.prevent_default(); props.onclick.call(());},
-                {props.icon}
+            button { class: "button-content {color_class}", disabled: props.disable, onclick: move |e| {e.prevent_default(); props.onclick.call(());},
+                if props.icon_side == IconSide::Left {
+                    {props.icon.clone()}
+                }
+
                 p {class: "button-text", "{props.text}"}
+
+                if props.icon_side == IconSide::Right {
+                    {props.icon.clone()}
+                }
             }
         }
     }
--- web/src/pages/login/mod.rs
+++ web/src/pages/login/mod.rs
@@ -1,8 +1,8 @@
-use dioxus::prelude::*;
+use dioxus::{logger::tracing, prelude::*};
 use gloo_storage::{LocalStorage, Storage};
 
 use crate::{
-    components::{Button, Subtitle, TextInput},
+    components::{Button, IconSide, Iconoir, Subtitle, TextInput},
     contexts::{auth::AuthContext, backend::BackendContext},
     pages::form::FormPageBase,
     router::Route,
@@ -13,20 +13,28 @@ pub fn LoginPage() -> Element {
     let navigator = navigator();
     let auth_ctx = use_context::<AuthContext>();
     let api_ctx = use_context::<BackendContext>();
+    let mut send_request = use_signal(|| false);
+    let mut falied_login = use_signal(|| false);
 
     if auth_ctx.token().read().is_some() {
         navigator.replace(Route::CorkBoardPage {});
     }
 
     // Email Input
-    let email_value = use_signal(|| String::new());
-    let email_error = use_signal(|| None);
-    let email_validate = move |value| {};
+    let mut email_value = use_signal(|| String::new());
+    let mut email_error = use_signal(|| None);
+    let email_validate = move |value| {
+        email_value.set(value);
+        email_error.set(None);
+    };
 
     // Password Input
-    let password_value = use_signal(|| String::new());
-    let password_error = use_signal(|| None);
-    let password_validate = move |value| {};
+    let mut password_value = use_signal(|| String::new());
+    let mut password_error = use_signal(|| None);
+    let password_validate = move |value| {
+        password_value.set(value);
+        password_error.set(None);
+    };
 
     // Button Input
     let on_click = move || {
@@ -35,16 +43,33 @@ pub fn LoginPage() -> Element {
         let nav = navigator.clone();
 
         async move {
-            let api = api;
-            let nav = nav;
-            let auth = auth;
-
             if email_error().is_some() || password_error().is_some() {
                 return;
             }
 
-            let res = api.login_user(email_value(), password_value()).await;
+            let email_input = email_value();
+            let password_input = password_value();
+            let mut is_error = false;
 
+            if email_input.is_empty() {
+                email_error.set(Some("E-mail não pode ser vazio.".into()));
+                is_error = true;
+            } else if !email_input.contains("@") {
+                email_error.set(Some("O Campo acima não e um E-mail valido.".into()));
+                is_error = true;
+            }
+
+            if password_input.is_empty() {
+                password_error.set(Some("Senha não pode ser vazio.".into()));
+                is_error = true;
+            }
+
+            if is_error {
+                return;
+            }
+
+            send_request.set(true);
+            let res = api.login_user(email_value(), password_value()).await;
             match res {
                 Ok(token) => {
                     let _ = LocalStorage::set("token", token.clone());
@@ -52,8 +77,17 @@ pub fn LoginPage() -> Element {
                     auth.token().set(Some(token));
                 }
 
-                Err(_e) => {}
+                Err(e) => {
+                    if &e.code == "INVALID_CREDENTIALS" {
+                        password_error.set(Some("Credencias Invalidas.".into()));
+                        falied_login.set(true);
+                    } else if &e.code == "USER_NOT_EXIST" {
+                        email_error.set(Some("Usuario não encontrado.".into()));
+                    }
+                }
             }
+
+            send_request.set(false);
         }
     };
 
@@ -87,15 +121,34 @@ pub fn LoginPage() -> Element {
 
                     div { class: "form-button",
                         Button {
-                            icon: rsx!{},
+                            disable: send_request(),
+                            icon: rsx! {
+                                if send_request() {
+                                    Iconoir {
+                                        icon: "hourglass",
+                                        class: "rotation-icon",
+                                    }
+                                } else {
+                                    {}
+                                }
+                            },
+                            icon_side: IconSide::Right,
                             text: "Acessar conta",
                             onclick: on_click
                         }
                     }
 
                     div { class: "form-footer",
-                        span { class: "small-text", "Não tem cadastro?" }
-                        Link{ class: "small-link", to: Route::RegisterPage{} , "Criar conta" }
+                        if falied_login() {
+                            div {
+                                a { class: "small-link", "Clique aqui para manda um E-mail de reset de senha" }
+                            }
+                        }
+
+                        div {
+                            span { class: "small-text", "Não tem cadastro?" }
+                            Link{ class: "small-link", to: Route::RegisterPage{} , "Criar conta" }
+                        }
                     }
                 }
             }
